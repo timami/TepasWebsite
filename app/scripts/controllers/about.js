b77d37613515434fbaa603995bc8097f97a3cb64
Emran Imami <Emran Imami>
2016-08-12 16:57:35 -0400

    first Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tepasWebsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tepasWebsiteApp
+ */
+angular.module('tepasWebsiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tepasWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tepasWebsiteApp
+ */
+angular.module('tepasWebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
